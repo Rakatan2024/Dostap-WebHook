7c5cf690f8ec5fe3c68fd92ac0d7116ae6767c13
Rakatan2024 <rakatan228322@gmail.com>
2024-02-25 20:02:42 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+)
+
+//func verifySignature(message, signature []byte, secret string) bool {
+//	mac := hmac.New(sha256.New, []byte(secret))
+//	mac.Write(message)
+//	expectedMAC := mac.Sum(nil)
+//	return hmac.Equal(signature, expectedMAC)
+//}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	cmd2 := exec.Command("../Dostap-Backend/deploy.sh")
+	err2 := cmd2.Run()
+	if err2 != nil {
+		fmt.Println(err2)
+		http.Error(w, "Failed to execute deploy script", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		fmt.Println("KEK:", err)
+	}
+}
