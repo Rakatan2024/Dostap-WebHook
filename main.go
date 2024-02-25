2dee8a7394928405f285b974caf69e2b0aef314e
Rakatan2024 <rakatan228322@gmail.com>
2024-02-25 20:19:57 +0600

    keki

--- main.go
+++ main.go
@@ -14,14 +14,17 @@ import (
 //}
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Zashel v handler")
 	cmd2 := exec.Command("../Dostap-Backend/deploy.sh")
 	err2 := cmd2.Run()
 	if err2 != nil {
+		fmt.Println("KEK ERR2")
 		fmt.Println(err2)
 		http.Error(w, "Failed to execute deploy script", http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
+	fmt.Println("VSE OKEY")
 }
 
 func main() {
